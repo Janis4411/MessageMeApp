13eecc477646f6eea914e044c3086fbdf3ba7e0e
Janis4411 <janis.vaneylen@gmail.com>
2022-04-01 17:08:02 +0200

    added log in/out function with bcrypt

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,7 +1,12 @@
 class ChatroomController < ApplicationController
 
   def index
-
+    @messages = Message.all
+    @users = User.all
   end
 
+  def show
+    @message = Message.find(params[:id])
+    @user = User.find(params[:id])
+  end
 end
