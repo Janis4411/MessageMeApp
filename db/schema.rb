7a139659654269a8914d52eadc10f6e5708b2b51
Janis4411 <janis.vaneylen@gmail.com>
2022-04-01 12:26:03 +0200

    added User model and Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_04_01_101913) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "kid", password_digest: "password1")
+User.create(username: "dude", password_digest: "password2")
+User.create(username: "lady", password_digest: "password3")
+User.create(username: "angela", password_digest: "password4")
+User.create(username: "someone", password_digest: "password5")
+User.create(username: "dragonkill0rhaxxor2005", password_digest: "password6")
+User.create(username: "Username", password_digest: "password7")
